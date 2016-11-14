ced9f2bb66364d456578f3e6cba2ee5545944c2e
kcsmta <tungkhanhmta@gmail.com>
2016-11-14 09:27:34 +0700

    Module Trang Chu

--- QUANLYNHANSU/DAO/DAO_DangNhap.cs
+++ QUANLYNHANSU/DAO/DAO_DangNhap.cs
@@ -17,40 +17,24 @@ namespace DAO
         public static IDataAdapter da;
         public static void Them(DTO_DangNhap gv)
         {
-            con = DAO_KetNoiDB.OpenConnect();
-            SqlHelper.ExecuteNonQuery(con, "PR_THEM_DANGNHAP", gv.UserName, gv.PassWord, gv.Quyen);
-            DAO_KetNoiDB.CloseConnect(con);
+            
         }
         public static DataTable HIENTHI_ALL()
         {
-            con = DAO_KetNoiDB.OpenConnect();
-            dt = SqlHelper.ExecuteDataset(con, "PR_HIENTHIDANGNHAP_ALL").Tables[0];
-            DAO_KetNoiDB.CloseConnect(con);
-            return dt;
+           
         }
         public static void Sua(DTO_DangNhap gv)
         {
-            con = DAO_KetNoiDB.OpenConnect();
-            SqlHelper.ExecuteNonQuery(con, "PR_SUA_DANGNHAP", gv.UserName, gv.PassWord,gv.Quyen);
-            DAO_KetNoiDB.CloseConnect(con);
+          
         }
 
         public static void Xoa(string gv)
         {
-            con = DAO_KetNoiDB.OpenConnect();
-            SqlHelper.ExecuteNonQuery(con, "PR_XOA_DANGNHAP", gv);
-            DAO_KetNoiDB.CloseConnect(con);
+            
         }
         public static int Xacthuc(string username, string pass)
         {
-            con = DAO_KetNoiDB.OpenConnect();
-            int role = -1;
-            con = DAO_KetNoiDB.OpenConnect();
-            dt = SqlHelper.ExecuteDataset(con, "PR_XACTHUC", username, pass).Tables[0];
-            if (dt.Rows.Count > 0)
-                role = (int)dt.Rows[0][0];
-            DAO_KetNoiDB.CloseConnect(con);
-            return role;
+           
         }
     }
 }
